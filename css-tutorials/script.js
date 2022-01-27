1d3b41c80bdd978a4a2eaf89595ddc2a87b3c684
ShivaliR <shivaliranka12@gmail.com>
2022-01-27 20:48:28 +0530

    My first commit

---
+++ css-tutorials/script.js
@@ -0,0 +1,22 @@
+// console.log("Inside script js file")
+// var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// var alertTrigger = document.getElementById('liveAlertBtn')
+
+// function alert(message, type) {
+//   var wrapper = document.createElement('div')
+//   wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+//   alertPlaceholder.append(wrapper)
+// }
+
+// if (alertTrigger) {
+//   alertTrigger.addEventListener('click', function () {
+//     alert('Nice, you triggered this alert message!', 'success')
+//   })
+// }
+// var myAlert = document.getElementById('myAlert')
+// myAlert.addEventListener('closed.bs.alert', function () {
+//   // do something, for instance, explicitly move focus to the most appropriate element,
+//   // so it doesn't get lost/reset to the start of the page
+//   // document.getElementById('...').focus()
+// })
